ec9f822b0aca61612e2cc74a20be51a3767c1d51
Harsh Khandelwal <khandelwalharsh401@gmail.com>
2022-10-12 20:33:16 +0530

    Create focal_length.cpp
    My solution to the problem "Focal length of spherical mirror"

---
+++ focal_length.cpp
@@ -0,0 +1,16 @@
+class Solution{
+	public:
+	int findFocalLength(float R, string type){
+	    float fl;
+	    if(type=="concave")
+	    {
+	        fl=(R/2);
+	        return floor(fl);
+	    }
+	    else 
+	    {
+	        fl=-(R/2);    
+	        return floor(fl);
+	    }
+	}
+};
