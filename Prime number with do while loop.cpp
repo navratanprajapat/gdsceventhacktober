a2034823065432f94dcdc8b062c28740bf72a46d
akshit-github <115628925+akshit-github@users.noreply.github.com>
2022-10-12 16:44:50 +0530

    Create Prime number with do while loop.cpp

---
+++ Prime number with do while loop.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,i,flag=0;
+	cout<<"Enter the number\n";
+	cin>>num;
+	i=2;
+	while(i<=num-1)
+	{
+		if(num%i==0)
+	
+	flag=1;
+	break;
+}
+i++;
+}
+     f(flag==0);
+{
+	cout<<num<<"is a prime number\n";
+	
+}
+else 
+{
+	cout<<num<<"is not a prime number\n";
+
+	
+	
+	return 0;
+}
